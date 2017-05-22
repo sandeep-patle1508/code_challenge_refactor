f8128c14d65eeb7c0478e7f638f6ccbe6a14387c
spatle-drg <spatle@dresources.com>
2017-05-22 11:48:40 +0530

    Add comments and correct indentation

--- refactoring/lib/file_utils.rb
+++ refactoring/lib/file_utils.rb
@@ -3,6 +3,7 @@
 # output:
 # - latest: get latest file on the basis of date from a folder
 # - sort: sort rows  by given key and write output into a new file
+
 class FileUtils
   # options for file read/write
   DEFAULT_CSV_OPTIONS = { col_sep: "\t", headers: :first_row }
@@ -12,31 +13,42 @@ class FileUtils
     @file = file
   end
 
+  # sort rows  by given key and write output into a new file
   def sort key
-		output = "#{@file}.sorted"
-		content_as_table = CSV.read(@file, DEFAULT_CSV_OPTIONS)
-		headers = content_as_table.headers
-		index_of_key = headers.index(key)
-		content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
-		write(content, headers, output)
-		return output
-	end
+    output = "#{@file}.sorted"
 
+    content_as_table = CSV.read(@file, DEFAULT_CSV_OPTIONS)
+    headers = content_as_table.headers
+    index_of_key = headers.index(key)
+
+    content = content_as_table.sort_by { |a| -a[index_of_key].to_i }
+    write(content, headers, output)
+
+    output
+  end
+
+  # get latest file on the basis of date from a folder
   def latest
     files = Dir["#{ ENV['HOME'] }/workspace/*#{@file}*.txt"]
+
     throw RuntimeError if files.empty?
+
     files.sort_by! do |file|
-  		last_date = /\d+-\d+-\d+_[[:alpha:]]+\.txt$/.match file
-  		last_date = last_date.to_s.match /\d+-\d+-\d+/
-  		DateTime.parse(last_date.to_s)
-  	end
-  	files.last
+      last_date = /\d+-\d+-\d+_[[:alpha:]]+\.txt$/.match file
+      last_date = last_date.to_s.match /\d+-\d+-\d+/
+      DateTime.parse(last_date.to_s)
+    end
+
+    files.last
   end
 
   private
+
+  # create new file as per given header and content
   def write(content, headers, output)
     CSV.open(output, 'wb', EXTENDED_CSV_OPTIONS) do |csv|
       csv << headers
+
       content.each do |row|
         csv << row
       end
--- refactoring/modifier.rb
+++ refactoring/modifier.rb
@@ -6,8 +6,15 @@ require_relative 'lib/file_utils'
 require 'csv'
 require 'date'
 
+## input:
+# - one file match string, sale_amount_factor and cancellation_factor
+# - get the latest file from array of files matched by given string
+# - sort the file rows by 'Clicks' column and modify the row values
+# output:
+# - create a sort and modified file
+
 class Modifier
-  # monkey-patch Float and String
+  # monkey-patch Float and String class
   Float.include CoreExtensions::Float
   String.include CoreExtensions::String
   # include constant
@@ -19,14 +26,17 @@ class Modifier
     @cancellation_factor = cancellation_factor
   end
 
+  # sort file by clicks column and genearte a new file
+  # modify the values of sorted file
+  # write output file
   def modify
-    # sort file by clicks column value
     sorted_file_enum = FileUtils.new(@input_file).sort('Clicks')
     modified_rows_enum = read_modify_rows(sorted_file_enum)
     generate_output_files(modified_rows_enum)
   end
 
   private
+
   # write modified rows into new file
   def generate_output_files(rows_enum)
     done = false
@@ -57,26 +67,29 @@ class Modifier
       end
     end
   end
+
   # modify values
   def modify_values(row)
     LAST_REAL_VALUE_WINS.each do |key|
       value = row[key]
       row[key] = (value.nil? or value.to_s == '0' or value == '') ? nil : value
     end
-    INT_VALUES.each do |key|
-      row[key] = row[key].to_s
-    end
-    FLOAT_VALUES.each do |key|
-      row[key] = row[key].from_german_to_f.to_german_s
-    end
+
+    INT_VALUES.each { |key| row[key] = row[key].to_s }
+
+    FLOAT_VALUES.each { |key| row[key] = row[key].from_german_to_f.to_german_s }
+
     COMMISSIONS.each do |key|
       row[key] = (@cancellation_factor * row[key].from_german_to_f).to_german_s
     end
+
     COMMISSIONS_VALUES.each do |key|
       row[key] = (@cancellation_factor * @sale_amount_factor * row[key].from_german_to_f).to_german_s
     end
+
     row
   end
+
   # lazy read and modify values
   def read_modify_rows(file)
     Enumerator.new do |yielder|
